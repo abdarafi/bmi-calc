3b80c25f82aeb80f7fc6b9b509aee6fbd53c3b88
Abda Rafi <abda_rafi@yahoo.com>
2020-04-15 20:38:41 +0700

    update for better ui

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple BMI Calculator
+Made only for my motivation of losing weight, thus I keep exercising. 💪
+
+### Installation
+1. clone this repo
+2. npm install
+3. start the project
+
+The calculation based on metric formula.🏋️
+
--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 //jshint esversion:6
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get("/", function(req, res) {
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-
+app.post("/", function (req, res) {
   var berat = Number(req.body.berat);
-  var tinggi = Math.pow(Number(req.body.tinggi), 2);
-  var hasil = berat / tinggi;
+  var tinggi = Number(req.body.tinggi) / 100;
+  var hasil = berat / Math.pow(tinggi, 2);
+  var typeBody = "";
   if (hasil < 18.5) {
-    res.send("Hasil BMI Anda adalah : " + hasil + "<br>Status berat badan Anda : <strong>Kurus.</strong>");
+    typeBody = "Skinny";
   } else if (hasil > 18.5 && hasil < 24.9) {
-    res.send("Hasil BMI Anda adalah : " + hasil + "<br>Status berat badan Anda : <strong>Normal.</strong>");
+    typeBody = "Normal";
+  } else if (hasil >= 25) {
+    typeBody = "Obese";
   } else {
-    res.send("Hasil BMI Anda adalah : " + hasil + "<br>Status berat badan Anda : <strong>Kegemukan.</strong>");
+    typeBody = "error";
   }
-
-
+  res.render("result", { hasilEjs: hasil, typeBodyEjs: typeBody });
 });
 
-app.listen("3000", function() {
-  console.log("Status: Berjalan pada port 3000.");
+app.listen("3000", function () {
+  console.log("Status: Running on port 3000.");
 });
